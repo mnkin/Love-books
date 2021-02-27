1c12a62726a3f0f31999651858b96bdbd3c7ae77
mnkin <2664306866@qq.com>
2021-02-27 21:34:58 +0800

    添加404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>您访问的页面不存在</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>

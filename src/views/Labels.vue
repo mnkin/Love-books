6b2eddef217103cd0e12ccc235116c975d0f51fd
mnkin <2664306866@qq.com>
2021-02-28 17:49:02 +0800

    解决了svg自带fill属性的问题

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -9,14 +9,4 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  display: flex;
-  border: 1px solid green;
-  flex-direction: column;
-  height: 100vh;
-}
-.content {
-  flex-grow: 1;
-  overflow: auto;
-}
 </style>

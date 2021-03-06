aa1e9b1707659a3b3d9b0779ee1132864ec9e3cb
mnkin <2664306866@qq.com>
2021-03-06 20:20:53 +0800

    封装出一个model

---
+++ src/model.ts
@@ -0,0 +1,24 @@
+const localStorageKeyName = "recordList";
+type RecordItem = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+  createdAt?: Date;
+};
+const model = {
+  clone(data: RecordItem[] | RecordItem) {
+    return JSON.parse(JSON.stringify(data));
+  },
+  fetch() {
+    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]") as RecordItem[];
+  },
+  save(data: RecordItem[]) {
+    window.localStorage.setItem(
+      localStorageKeyName,
+      JSON.stringify(data)
+    );
+  },
+};
+
+export default model;

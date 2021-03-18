82b4f676a53d17a2f25b2b8473cbaf06481fc046
mnkin <2664306866@qq.com>
2021-03-18 21:14:17 +0800

    解决id生成问题

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -51,9 +51,11 @@ export default class EditLabel extends Vue {
       tagListModel.update(this.tag.id, name);
     }
   }
-  remove(id: string) {
+  remove() {
     if (this.tag) {
-      tagListModel.remove(this.tag.id);
+      if (tagListModel.remove(this.tag.id)) {
+        this.$router.back();
+      } else window.alert("删除失败");
     }
   }
   goBack() {

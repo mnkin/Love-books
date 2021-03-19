45ed70f035c7199f05d58513a870acd26ddb35a0
mnkin <2664306866@qq.com>
2021-03-19 22:46:51 +0800

    使用全局tagList

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -7,3 +7,27 @@ declare module "*.svg" {
   const content: string;
   export default content;
 }
+
+type RecordItem = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+  createdAt?: Date;
+};
+type Tag = {
+  id: string;
+  name: string;
+};
+type TagListModel = {
+  data: Tag[];
+  fetch: () => Tag[];
+  create: (name: string) => 'success' | 'duplicated'; //联合类型
+  remove: (id: string) => boolean;
+  update: (id: string, name: string) => 'success' | "not found" | "duplicated";
+  save: () => void;
+};
+
+interface Window {
+  tagList: Tag[];
+}
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -26,7 +26,6 @@ import recordListModel from "@/models/recordListModel";
 import tagListModel from "../models/tagListModel";
 
 const recordList = recordListModel.fetch();
-const tagList = tagListModel.fetch();
 
 type RecordItem = {
   tags: string[];
@@ -40,7 +39,8 @@ type RecordItem = {
   components: { Layout, Tags, FormItem, Types, NumberPad },
 })
 export default class Money extends Vue {
-  tags = tagList;
+  tags = window.tagList;
+  
   recordList: RecordItem[] = recordList;
   record: RecordItem = { tags: [], notes: "", type: "-", amount: 0 };
 

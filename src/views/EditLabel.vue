28f39015bcb5459d2ef29f8557136019159c537f
mnkin <2664306866@qq.com>
2021-03-25 21:42:19 +0800

    EditLabel.vue使用Vuex

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -24,16 +24,18 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import FormItem from "@/components/Money/FormItem.vue";
 import Button from "../components/Button.vue";
+import tagStore from "../store/tagStore";
 
 @Component({
   components: { FormItem, Button },
 })
 export default class EditLabel extends Vue {
-  tag?: Tag = undefined;
-
+  get tag() {
+    return this.$store.state.currentTag;
+  }
   created() {
-    //   TODO
-    // const tag = store.findTag(this.$route.params.id);
+    const id = this.$route.params.id;
+    this.$store.commit("setCurrentTag", id);
     if (!this.tag) {
       this.$router.replace("/404");
     }
